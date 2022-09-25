98c98addd92bc69d8fbc6acfb29895ff28b9ce3f
sean <sean165@outlook.de>
2022-09-26 00:10:11 +0200

    Fix: Lower CMake minimum required version
    Also fixes a multitude of CMake issues

--- cmake/add_source_directory.cmake
+++ cmake/add_source_directory.cmake
@@ -3,8 +3,9 @@
 function(add_source_directory)
     cmake_parse_arguments(PARAM "" "TARGET;FOLDER" "CONDITIONAL" ${ARGN})
 
-    file(GLOB TARGET_SOURCES ${PARAM_FOLDER}/*.c ${PARAM_FOLDER}/*.cpp ${PARAM_FOLDER}/*.mm)
-    file(GLOB TARGET_HEADERS ${PARAM_FOLDER}/*.h ${PARAM_FOLDER}/*.hpp)
+    # Generic C/C++/ObjC file extensions
+    file(GLOB TARGET_SOURCES ${PARAM_FOLDER}/*.c ${PARAM_FOLDER}/*.cpp ${PARAM_FOLDER}/*.cc ${PARAM_FOLDER}/*.cxx ${PARAM_FOLDER}/*.m ${PARAM_FOLDER}/*.mm)
+    file(GLOB TARGET_HEADERS ${PARAM_FOLDER}/*.h ${PARAM_FOLDER}/*.hpp ${PARAM_FOLDER}/*.hh)
 
     foreach(SOURCE ${TARGET_SOURCES})
         target_sources(${PARAM_TARGET} PRIVATE ${SOURCE})
