b0bbdfe803f3708fd4efd10d3e094dd12ef57140
sean <sean165@outlook.de>
2022-12-24 19:06:18 +0100

    Change: Always enable function inlining

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -9,8 +9,22 @@ macro(compiler_flags)
     # cpuid, meaning no architecture flags or other compile flags need to be passed.
     # See https://github.com/simdjson/simdjson/blob/master/doc/implementation-selection.md.
     if (MSVC)
-        target_compile_options(${PARAM_TARGET} PRIVATE /EHsc $<$<CONFIG:RELEASE>:/O2 /Ob2 /Ot>)
+        target_compile_options(${PARAM_TARGET} PRIVATE /EHsc $<$<CONFIG:RELEASE>:/O2 /Ob3 /Ot>)
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         target_compile_options(${PARAM_TARGET} PRIVATE $<$<CONFIG:RELEASE>:-O3>)
+    endif()
+endmacro()
+
+macro(enable_debug_inlining)
+    cmake_parse_arguments(PARAM "" "TARGET" "" ${ARGN})
+
+    if (PARAM_TARGET STREQUAL "" OR NOT TARGET ${PARAM_TARGET})
+        return()
+    endif()
+
+    if (MSVC)
+        target_compile_options(${PARAM_TARGET} PRIVATE $<$<CONFIG:DEBUG>:/Ob2>)
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        target_compile_options(${PARAM_TARGET} PRIVATE $<$<CONFIG:DEBUG>:-finline-functions>)
     endif()
 endmacro()
