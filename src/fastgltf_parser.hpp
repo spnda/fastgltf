122e938bc2dd600591d7c836e478f8dc68aafb7c
David Dubois <davidduboisengineer@gmail.com>
2023-03-29 08:32:47 -0700

    Add: Android APK file load

--- src/fastgltf_parser.hpp
+++ src/fastgltf_parser.hpp
@@ -45,6 +45,10 @@
 #endif
 
 // fwd
+#if defined(__ANDROID__)
+struct AAssetManager;
+#endif
+
 namespace simdjson::dom {
     class array;
     class object;
@@ -280,6 +284,7 @@ namespace fastgltf {
         friend class Parser;
         friend GltfType determineGltfFileType(GltfDataBuffer* buffer);
 
+    protected:
         std::size_t allocatedSize = 0;
         std::size_t dataSize = 0;
         std::byte* bufferPointer = nullptr;
@@ -290,7 +295,7 @@ namespace fastgltf {
 
     public:
         explicit GltfDataBuffer() noexcept;
-        ~GltfDataBuffer() noexcept;
+        virtual ~GltfDataBuffer() noexcept;
 
         /**
          * Saves the pointer including its range. Does not copy any data. This requires the
@@ -321,6 +326,21 @@ namespace fastgltf {
             return span<std::byte>(bufferPointer, dataSize);
         }
     };
+
+    #if defined(__ANDROID__)
+    class AndroidGltfDataBuffer : public GltfDataBuffer {
+        AAssetManager* assetManager;
+
+    public:
+        explicit AndroidGltfDataBuffer(AAssetManager* assetManager) noexcept;
+        ~AndroidGltfDataBuffer() noexcept = default;
+
+        /**
+         * Loads a file from within an Android APK
+         */
+        bool loadFromAndroidAsset(const std::filesystem::path& path, std::uint64_t byteOffset = 0) noexcept;
+    };
+    #endif
 
     /**
      * Some internals the parser passes on to each glTF instance.
