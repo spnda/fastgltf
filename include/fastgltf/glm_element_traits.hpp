cc07cd620067b3e5380eaab5cdee6797a0c08871
sean <sean165@outlook.de>
2023-07-06 17:52:45 +0200

    Fix: Double-namespaced glm ElementTraits

--- include/fastgltf/glm_element_traits.hpp
+++ include/fastgltf/glm_element_traits.hpp
@@ -11,66 +11,66 @@
 namespace fastgltf {
 
 template<>
-struct fastgltf::ElementTraits<glm::vec2> : fastgltf::ElementTraitsBase<glm::vec2, AccessorType::Vec2, float> {};
+struct ElementTraits<glm::vec2> : ElementTraitsBase<glm::vec2, AccessorType::Vec2, float> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::vec3> : fastgltf::ElementTraitsBase<glm::vec3, AccessorType::Vec3, float> {};
+struct ElementTraits<glm::vec3> : ElementTraitsBase<glm::vec3, AccessorType::Vec3, float> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::vec4> : fastgltf::ElementTraitsBase<glm::vec4, AccessorType::Vec4, float> {};
+struct ElementTraits<glm::vec4> : ElementTraitsBase<glm::vec4, AccessorType::Vec4, float> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::i8vec2> : fastgltf::ElementTraitsBase<glm::i8vec2, AccessorType::Vec2, std::int8_t> {};
+struct ElementTraits<glm::i8vec2> : ElementTraitsBase<glm::i8vec2, AccessorType::Vec2, std::int8_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::i8vec3> : fastgltf::ElementTraitsBase<glm::i8vec3, AccessorType::Vec3, std::int8_t> {};
+struct ElementTraits<glm::i8vec3> : ElementTraitsBase<glm::i8vec3, AccessorType::Vec3, std::int8_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::i8vec4> : fastgltf::ElementTraitsBase<glm::i8vec4, AccessorType::Vec4, std::int8_t> {};
+struct ElementTraits<glm::i8vec4> : ElementTraitsBase<glm::i8vec4, AccessorType::Vec4, std::int8_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u8vec2> : fastgltf::ElementTraitsBase<glm::u8vec2, AccessorType::Vec2, std::uint8_t> {};
+struct ElementTraits<glm::u8vec2> : ElementTraitsBase<glm::u8vec2, AccessorType::Vec2, std::uint8_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u8vec3> : fastgltf::ElementTraitsBase<glm::u8vec3, AccessorType::Vec3, std::uint8_t> {};
+struct ElementTraits<glm::u8vec3> : ElementTraitsBase<glm::u8vec3, AccessorType::Vec3, std::uint8_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u8vec4> : fastgltf::ElementTraitsBase<glm::u8vec4, AccessorType::Vec4, std::uint8_t> {};
+struct ElementTraits<glm::u8vec4> : ElementTraitsBase<glm::u8vec4, AccessorType::Vec4, std::uint8_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::i16vec2> : fastgltf::ElementTraitsBase<glm::i16vec2, AccessorType::Vec2, std::int16_t> {};
+struct ElementTraits<glm::i16vec2> : ElementTraitsBase<glm::i16vec2, AccessorType::Vec2, std::int16_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::i16vec3> : fastgltf::ElementTraitsBase<glm::i16vec3, AccessorType::Vec3, std::int16_t> {};
+struct ElementTraits<glm::i16vec3> : ElementTraitsBase<glm::i16vec3, AccessorType::Vec3, std::int16_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::i16vec4> : fastgltf::ElementTraitsBase<glm::i16vec4, AccessorType::Vec4, std::int16_t> {};
+struct ElementTraits<glm::i16vec4> : ElementTraitsBase<glm::i16vec4, AccessorType::Vec4, std::int16_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u16vec2> : fastgltf::ElementTraitsBase<glm::u16vec2, AccessorType::Vec2, std::uint16_t> {};
+struct ElementTraits<glm::u16vec2> : ElementTraitsBase<glm::u16vec2, AccessorType::Vec2, std::uint16_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u16vec3> : fastgltf::ElementTraitsBase<glm::u16vec3, AccessorType::Vec3, std::uint16_t> {};
+struct ElementTraits<glm::u16vec3> : ElementTraitsBase<glm::u16vec3, AccessorType::Vec3, std::uint16_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u16vec4> : fastgltf::ElementTraitsBase<glm::u16vec4, AccessorType::Vec4, std::uint16_t> {};
+struct ElementTraits<glm::u16vec4> : ElementTraitsBase<glm::u16vec4, AccessorType::Vec4, std::uint16_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u32vec2> : fastgltf::ElementTraitsBase<glm::u32vec2, AccessorType::Vec2, std::uint32_t> {};
+struct ElementTraits<glm::u32vec2> : ElementTraitsBase<glm::u32vec2, AccessorType::Vec2, std::uint32_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u32vec3> : fastgltf::ElementTraitsBase<glm::u32vec3, AccessorType::Vec3, std::uint32_t> {};
+struct ElementTraits<glm::u32vec3> : ElementTraitsBase<glm::u32vec3, AccessorType::Vec3, std::uint32_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::u32vec4> : fastgltf::ElementTraitsBase<glm::u32vec4, AccessorType::Vec4, std::uint32_t> {};
+struct ElementTraits<glm::u32vec4> : ElementTraitsBase<glm::u32vec4, AccessorType::Vec4, std::uint32_t> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::mat2> : fastgltf::ElementTraitsBase<glm::mat2, AccessorType::Mat2, float> {};
+struct ElementTraits<glm::mat2> : ElementTraitsBase<glm::mat2, AccessorType::Mat2, float> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::mat3> : fastgltf::ElementTraitsBase<glm::mat3, AccessorType::Mat3, float> {};
+struct ElementTraits<glm::mat3> : ElementTraitsBase<glm::mat3, AccessorType::Mat3, float> {};
 
 template<>
-struct fastgltf::ElementTraits<glm::mat4> : fastgltf::ElementTraitsBase<glm::mat4, AccessorType::Mat4, float> {};
+struct ElementTraits<glm::mat4> : ElementTraitsBase<glm::mat4, AccessorType::Mat4, float> {};
 
 } // namespace fastgltf
