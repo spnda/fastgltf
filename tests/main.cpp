062810008a18fd58e791204f3e33eaafed1e333a
sean <sean165@outlook.de>
2024-04-21 19:07:42 +0200

    Change: Set maximum proc priority for Unix/MacOS

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,14 +2,19 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#elif defined(__APPLE__) || defined(unix)
+#include <sys/resource.h>
+#include <unistd.h>
 #endif
 
 // See https://github.com/catchorg/Catch2/blob/v3.5.4/docs/own-main.md
 int main(int argc, char* argv[]) {
-#if defined(_WIN32) && defined(NDEBUG)
 	// Guarantee the best performance and result stability across runs
+#if defined(_WIN32) && defined(NDEBUG)
 	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
 	SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
+#elif defined(__APPLE__) || defined(unix)
+	setpriority(PRIO_PROCESS, getpid(), PRIO_MAX);
 #endif
 	return Catch::Session().run(argc, argv);
 }
