e70bc842d279749c363e9d92c038e65478d42a88
sean <sean165@outlook.de>
2024-07-25 19:57:32 +0200

    Bump version to 0.8.0

--- include/fastgltf/types.hpp
+++ include/fastgltf/types.hpp
@@ -99,7 +99,7 @@
 #define FASTGLTF_QUOTE(x) FASTGLTF_QUOTE_Q(x)
 
 // fastgltf version string. Use FASTGLTF_QUOTE to stringify.
-#define FASTGLTF_VERSION 0.7.1
+#define FASTGLTF_VERSION 0.8.0
 
 namespace fastgltf {
 #if defined(FASTGLTF_USE_64BIT_FLOAT) && FASTGLTF_USE_64BIT_FLOAT
